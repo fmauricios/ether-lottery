eb7fe437c253daa06154b9a91b0a034c66a549fb
Mauricio Serna Florez <mauricioserf@gmail.com>
2018-03-06 13:30:23 -0500

    [Add] Lottery contract, remove old Inbox test content.

---
+++ config.example.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mnemonic: 'copy your 12 words here copy your 12 words here here here',
+  networkUrl: 'networkUrl - Example: Rinkeby, Ropsten. Infura'
+}
---
+++ test/Lottery.test.js
@@ -0,0 +1,16 @@
+/*
+ * Module dependencies
+ */
+
+const assert = require('assert')
+const ganache = require('ganache-cli')
+const Web3 = require('web3')
+
+const provider = ganache.provider()
+const web3 = new Web3(provider)
+
+const { interface, bytecode } = require('../compile')
+
+let accounts = null
+let inbox = null
+
